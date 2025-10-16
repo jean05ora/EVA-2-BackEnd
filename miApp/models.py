5120f810896a1d3349fe89cfc71890bd66c08c8a
jean05ora <jean.diaz05@hotmail.com>
2025-10-16 16:39:19 -0300

    Modelos agregados

---
+++ miApp/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+
+class Polera(models.Model):
+    nombre_polera = models.CharField(max_length=155)
+    precio_polera = models.PositiveIntegerField()
+    stock_polera = models.PositiveIntegerField()
+    descripcion_polera = models.TextField()
+    imagen_polera = models.CharField(max_length=555)
+
+    def __str__(self):
+        return self.nombre_polera
+
+
+class Fragancia(models.Model):
+    nombre_fragancia = models.CharField(max_length=155)
+    precio_fragancia = models.PositiveIntegerField()
+    stock_fragancia = models.PositiveIntegerField()
+    descripcion_fragancia = models.TextField()
+    imagen_fragancia = models.CharField(max_length=555)
+
+    def __str__(self):
+        return self.nombre_fragancia
