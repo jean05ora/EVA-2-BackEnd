cc4cb7fd712b9566fa37e0011e46b80e63e72e4b
jean05ora <jean.diaz05@hotmail.com>
2025-10-16 16:53:44 -0300

    .

--- miApp/views.py
+++ miApp/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
+from .models import Polera, Fragancia
 
-# Create your views here.
+def main(request):
+    return render(request, "main.html")
+
+def poleras(request):
+    context = {}
+
+    poleras = Polera.objects.all
+
+    context['poleras'] = poleras
+
+    return render(request,"poleras.html", context)
+
+def fragancias(request):
+    context = {}
+
+    fragancias = Fragancia.objects.all
+
+    context['fragancias'] = fragancias
+
+    return render(request,"fragancias.html", context)
+
+def detalles_productos(request, id):
+    context = {}
+
+    poleras = Polera.objects.get(id=id)
+    fragancias = Fragancia.objects.get(id=id)
+
+    context['poleras'] = poleras 
+    context['fragancias'] = fragancias
+
+    return render(request, "detalles_producto.html", context)
